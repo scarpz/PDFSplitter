35fa715b9c95a6ecfcddefff9fca57742e1bd60a
Scarpz <felipe.scarpz@gmail.com>
2019-05-22 21:02:26 +0100

    Minor

--- PDFSplitter/ViewController.swift
+++ PDFSplitter/ViewController.swift
@@ -17,25 +17,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let url = Bundle.main.url(forResource: "Document", withExtension: "pdf")!
-        
-        self.pdf = PDFServices.loadPDF(from: url)
-    }
-    
-    @IBAction func splitPDF(_ sender: UIButton) {
-        PDFServices.splitPages(from: self.pdf)
-    }
-    
-    @IBAction func createPDFFromRange(_ sender: UIButton) {
-        PDFServices.createNewPDF(from: self.pdf, firstPage: 10, lastPage: 20)
-    }
-    
-    @IBAction func loadPDF(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func shareAllPages(_ sender: UIBarButtonItem) {
-//        let share = UIActivityViewController(activityItems: self.allPages, applicationActivities: nil)
-//
-//        self.present(share, animated: true, completion: nil)
     }
 }
