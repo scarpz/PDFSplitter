f814b313c3c654c057643ce8c21f8c66fc4ecad2
Scarpz <felipe.scarpz@gmail.com>
2019-05-24 00:08:07 +0100

    PDF retrieve in extension viewDidLoad

---
+++ PDFSplitterExt/ActionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ActionViewController.swift
+//  PDFSplitterExt
+//
+//  Created by Scarpz on 23/05/19.
+//  Copyright © 2019 Felipe Scarpitta. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import PDFKit
+
+class ActionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fileItem = self.extensionContext!.inputItems.first as! NSExtensionItem
+        let itemProvider = fileItem.attachments!.first!
+        let identifier = kUTTypePDF as String
+        
+        if itemProvider.hasItemConformingToTypeIdentifier(identifier) {
+            
+            itemProvider.loadItem(forTypeIdentifier: identifier, options: nil) { pdfURL, error in
+                
+                if let pdfURL = pdfURL as? URL {
+                    let pdf = PDFDocument(url: pdfURL)
+                    print(pdf)
+                }
+            }
+        }
+    }
+    
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
